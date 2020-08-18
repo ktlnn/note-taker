f94fd1e393e6d7a6311cc4eeaf91895698071f32
Katelynn <ktlnn.nguyen@gmail.com>
2020-08-17 22:21:44 -0400

    configuring yarn/npm

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,3 +7,12 @@ const util = require("util");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+
+
+router.get("/api/notes", (req, res => {
+    res.sendFile(path.join(__dirname, "../db/", "db.json"));
+}));
+
+router.post("/api/notes", (req, res => {
+    console.log(res);
+}));
